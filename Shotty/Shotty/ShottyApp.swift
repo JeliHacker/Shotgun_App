e508c4e125000fbebd08bfa76454f9ab0811bdb2
JeliHacker <jeligooch@gmail.com>
2022-11-14 00:36:11 +0100

    initial commit, just one image file added

---
+++ Shotty/Shotty/ShottyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShottyApp.swift
+//  Shotty
+//
+//  Created by James Elisha Gooch on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ShottyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
